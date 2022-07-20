ca63943a1cc4c3562984e8f90ef855ffb696d2c6
Nataly Siganova <siganova.tihanskaya@gmail.com>
2022-07-21 00:10:33 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = ingredients.map(ingredient => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add('item');
+  return ingredientEl;
+});
+
+const ingredientsContainer = document.getElementById('ingredients');
+ingredientsContainer.append(...ingredientsEl);
